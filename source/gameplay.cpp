4cae9d9cd5cff89ffaed1089bd2da353fa5004a6
André Silva <andre@Linux-Andre.home>
2016-08-15 17:06:58 +0100

    Finished all gameplay mode controls and widgets.

--- source/gameplay.cpp
+++ source/gameplay.cpp
@@ -7,7 +7,11 @@ Gameplay::Gameplay(Game* game) :
     Game_State(game),
     game_mode(GAME_MODE_BEGINNER),
     mouse_on_ok_button(false),
-    chosen_team(TEAM_NONE) ,
+    chosen_team(TEAM_NONE),
+    picker_team_x(0),
+    mouse_down_on_team_picker(false),
+    picker_unclaimed_x(0),
+    mouse_down_on_unclaimed_picker(false),
     bmp_button_1(nullptr),
     bmp_button_2(nullptr),
     bmp_picker_1(nullptr),
@@ -31,6 +35,13 @@ void Gameplay::load() {
     bmp_picker_2 = game->bmp_mgr.get_bitmap(BMP_PICKER_2);
     bmp_picker_3 = game->bmp_mgr.get_bitmap(BMP_PICKER_3);
     bmp_ink_effect = game->bmp_mgr.get_bitmap(BMP_INK_EFFECT);
+    
+    if(game_mode == GAME_MODE_INTERMEDIATE) {
+        picker_team_x = PICKER_I_BAR_W / 2.0;
+    } else if(game_mode == GAME_MODE_EXPERT) {
+        picker_team_x = PICKER_E_TEAM_BAR_W / 2.0;
+        picker_unclaimed_x = PICKER_E_NONE_BAR_W / 4.0;
+    }
 }
 
 
@@ -45,25 +56,101 @@ void Gameplay::handle_mouse(ALLEGRO_EVENT ev) {
     
         if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
             if(
-                ev.mouse.y >= PICKER_1_TEAM_BUTTON_Y &&
-                ev.mouse.y <= PICKER_1_TEAM_BUTTON_Y + PICKER_1_TEAM_BUTTON_H
+                ev.mouse.y >= PICKER_B_TEAM_BUTTON_Y &&
+                ev.mouse.y <= PICKER_B_TEAM_BUTTON_Y + PICKER_B_TEAM_BUTTON_H
             ) {
                 if(
-                    ev.mouse.x >= PICKER_1_TEAM_1_BUTTON_X &&
-                    ev.mouse.x <= PICKER_1_TEAM_1_BUTTON_X +
-                    PICKER_1_TEAM_BUTTON_W
+                    ev.mouse.x >= PICKER_B_TEAM_1_BUTTON_X &&
+                    ev.mouse.x <= PICKER_B_TEAM_1_BUTTON_X +
+                    PICKER_B_TEAM_BUTTON_W
                 ) {
                     chosen_team = TEAM_1;
                     
                 } else if(
-                    ev.mouse.x >= PICKER_1_TEAM_2_BUTTON_X &&
-                    ev.mouse.x <= PICKER_1_TEAM_2_BUTTON_X +
-                    PICKER_1_TEAM_BUTTON_W
+                    ev.mouse.x >= PICKER_B_TEAM_2_BUTTON_X &&
+                    ev.mouse.x <= PICKER_B_TEAM_2_BUTTON_X +
+                    PICKER_B_TEAM_BUTTON_W
                 ) {
                     chosen_team = TEAM_2;
                     
                 }
             }
+        }
+        
+    } else if(game_mode == GAME_MODE_INTERMEDIATE) {
+    
+        if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
+            if(
+                ev.mouse.x >= PICKER_I_X && ev.mouse.y >= PICKER_I_BAR_Y &&
+                ev.mouse.x <= PICKER_I_X + PICKER_I_W &&
+                ev.mouse.y <= PICKER_I_BAR_Y + PICKER_I_BAR_H
+            ) {
+                mouse_down_on_team_picker = true;
+                update_picker_x(
+                    &picker_team_x, ev.mouse.x,
+                    PICKER_I_BAR_X, PICKER_I_BAR_W, true
+                );
+            }
+            
+        } else if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP) {
+            mouse_down_on_team_picker = false;
+            
+        } else if(ev.type == ALLEGRO_EVENT_MOUSE_AXES) {
+            if(mouse_down_on_team_picker) {
+                update_picker_x(
+                    &picker_team_x, ev.mouse.x,
+                    PICKER_I_BAR_X, PICKER_I_BAR_W, true
+                );
+            }
+            
+        }
+        
+    } else {
+    
+        if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
+            if(
+                ev.mouse.x >= PICKER_E_X &&
+                ev.mouse.y >= PICKER_E_TEAM_BAR_Y &&
+                ev.mouse.x <= PICKER_E_X + PICKER_E_W &&
+                ev.mouse.y <= PICKER_E_TEAM_BAR_Y + PICKER_E_TEAM_BAR_H
+            ) {
+                mouse_down_on_team_picker = true;
+                update_picker_x(
+                    &picker_team_x, ev.mouse.x,
+                    PICKER_E_TEAM_BAR_X, PICKER_E_TEAM_BAR_W, true
+                );
+                
+            } else if(
+                ev.mouse.x >= PICKER_E_X &&
+                ev.mouse.y >= PICKER_E_NONE_BAR_Y &&
+                ev.mouse.x <= PICKER_E_X + PICKER_E_W &&
+                ev.mouse.y <= PICKER_E_NONE_BAR_Y + PICKER_E_NONE_BAR_H
+            ) {
+                mouse_down_on_unclaimed_picker = true;
+                update_picker_x(
+                    &picker_unclaimed_x, ev.mouse.x,
+                    PICKER_E_NONE_BAR_X, PICKER_E_NONE_BAR_W, false
+                );
+                
+            }
+            
+        } else if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP) {
+            mouse_down_on_team_picker = false;
+            mouse_down_on_unclaimed_picker = false;
+            
+        } else if(ev.type == ALLEGRO_EVENT_MOUSE_AXES) {
+            if(mouse_down_on_team_picker) {
+                update_picker_x(
+                    &picker_team_x, ev.mouse.x,
+                    PICKER_E_TEAM_BAR_X, PICKER_E_TEAM_BAR_W, true
+                );
+            } else if(mouse_down_on_unclaimed_picker) {
+                update_picker_x(
+                    &picker_unclaimed_x, ev.mouse.x,
+                    PICKER_E_NONE_BAR_X, PICKER_E_TEAM_BAR_W, false
+                );
+            }
+            
         }
         
     }
@@ -89,8 +176,8 @@ void Gameplay::do_drawing() {
     
     if(game_mode == GAME_MODE_BEGINNER) {
         draw_ink_button(
-            PICKER_1_TEAM_1_BUTTON_X, PICKER_1_TEAM_BUTTON_Y,
-            PICKER_1_TEAM_BUTTON_W, PICKER_1_TEAM_BUTTON_H,
+            PICKER_B_TEAM_1_BUTTON_X, PICKER_B_TEAM_BUTTON_Y,
+            PICKER_B_TEAM_BUTTON_W, PICKER_B_TEAM_BUTTON_H,
             (
                 chosen_team == TEAM_1 ?
                 cur_arena.ink_colors[TEAM_1] :
@@ -100,8 +187,8 @@ void Gameplay::do_drawing() {
         );
         
         draw_ink_button(
-            PICKER_1_TEAM_2_BUTTON_X, PICKER_1_TEAM_BUTTON_Y,
-            PICKER_1_TEAM_BUTTON_W, PICKER_1_TEAM_BUTTON_H,
+            PICKER_B_TEAM_2_BUTTON_X, PICKER_B_TEAM_BUTTON_Y,
+            PICKER_B_TEAM_BUTTON_W, PICKER_B_TEAM_BUTTON_H,
             (
                 chosen_team == TEAM_2 ?
                 cur_arena.ink_colors[TEAM_2] :
@@ -110,10 +197,85 @@ void Gameplay::do_drawing() {
             (chosen_team == TEAM_2)
         );
         
-        al_draw_bitmap(bmp_picker_1, PICKER_1_X, PICKER_1_Y, 0);
+        al_draw_bitmap(bmp_picker_1, PICKER_B_X, PICKER_B_Y, 0);
         
         ALLEGRO_COLOR ok_button_tint = al_map_rgb(255, 255, 255);
-        if(chosen_team != 2) {
+        if(chosen_team != TEAM_NONE) {
+            ok_button_tint = cur_arena.ink_colors[chosen_team];
+        }
+        al_draw_tinted_bitmap(
+            (mouse_on_ok_button ? bmp_button_2 : bmp_button_1),
+            ok_button_tint, OK_BUTTON_X, OK_BUTTON_Y, 0
+        );
+        
+    } else if(game_mode == GAME_MODE_INTERMEDIATE) {
+        draw_ink_button(
+            PICKER_I_BAR_X, PICKER_I_BAR_Y,
+            picker_team_x, PICKER_I_BAR_H,
+            (
+                chosen_team != TEAM_NONE ?
+                cur_arena.ink_colors[TEAM_1] :
+                darken_color(cur_arena.ink_colors[TEAM_1], 0.5)
+            ),
+            chosen_team != TEAM_NONE
+        );
+        
+        draw_ink_button(
+            PICKER_I_BAR_X + picker_team_x, PICKER_I_BAR_Y,
+            PICKER_I_BAR_W - picker_team_x, PICKER_I_BAR_H,
+            (
+                chosen_team != TEAM_NONE ?
+                cur_arena.ink_colors[TEAM_2] :
+                darken_color(cur_arena.ink_colors[TEAM_2], 0.5)
+            ),
+            chosen_team != TEAM_NONE
+        );
+        
+        al_draw_bitmap(bmp_picker_2, PICKER_I_X, PICKER_I_Y, 0);
+        
+        ALLEGRO_COLOR ok_button_tint = al_map_rgb(255, 255, 255);
+        if(chosen_team != TEAM_NONE) {
+            ok_button_tint = cur_arena.ink_colors[chosen_team];
+        }
+        al_draw_tinted_bitmap(
+            (mouse_on_ok_button ? bmp_button_2 : bmp_button_1),
+            ok_button_tint, OK_BUTTON_X, OK_BUTTON_Y, 0
+        );
+        
+    } else if(game_mode == GAME_MODE_EXPERT) {
+        draw_ink_button(
+            PICKER_E_TEAM_BAR_X, PICKER_E_TEAM_BAR_Y,
+            picker_team_x, PICKER_E_TEAM_BAR_H,
+            (
+                chosen_team != TEAM_NONE ?
+                cur_arena.ink_colors[TEAM_1] :
+                darken_color(cur_arena.ink_colors[TEAM_1], 0.5)
+            ),
+            chosen_team != TEAM_NONE
+        );
+        
+        draw_ink_button(
+            PICKER_E_TEAM_BAR_X + picker_team_x, PICKER_E_TEAM_BAR_Y,
+            PICKER_E_TEAM_BAR_W - picker_team_x, PICKER_E_TEAM_BAR_H,
+            (
+                chosen_team != TEAM_NONE ?
+                cur_arena.ink_colors[TEAM_2] :
+                darken_color(cur_arena.ink_colors[TEAM_2], 0.5)
+            ),
+            chosen_team != TEAM_NONE
+        );
+        
+        al_draw_filled_rectangle(
+            PICKER_E_NONE_BAR_X, PICKER_E_NONE_BAR_Y,
+            PICKER_E_NONE_BAR_X + picker_unclaimed_x,
+            PICKER_E_NONE_BAR_Y + PICKER_E_NONE_BAR_H,
+            al_map_rgb(128, 128, 128)
+        );
+        
+        al_draw_bitmap(bmp_picker_3, PICKER_E_X, PICKER_E_Y, 0);
+        
+        ALLEGRO_COLOR ok_button_tint = al_map_rgb(255, 255, 255);
+        if(chosen_team != TEAM_NONE) {
             ok_button_tint = cur_arena.ink_colors[chosen_team];
         }
         al_draw_tinted_bitmap(
@@ -145,8 +307,8 @@ void Gameplay::draw_ink_button(
     
     vertexes[0].x = x;
     vertexes[0].y = y;
-    vertexes[0].u = (moving ? -(game->time_spent * 10) : 0);
-    vertexes[0].v = (moving ? -(game->time_spent * 20) : 0);
+    vertexes[0].u = x + (moving ? -(game->time_spent * 10) : 0);
+    vertexes[0].v = y + (moving ? -(game->time_spent * 20) : 0);
     
     vertexes[1].x = vertexes[0].x + w;
     vertexes[1].y = vertexes[0].y;
@@ -166,4 +328,17 @@ void Gameplay::draw_ink_button(
     al_draw_prim(
         vertexes, NULL, bmp_ink_effect, 0, 4, ALLEGRO_PRIM_TRIANGLE_FAN
     );
+}
+
+
+void Gameplay::update_picker_x(
+    int* var, const int mouse_x,
+    const int bar_x, const int bar_w, const bool update_chosen_team
+) {
+    *var = mouse_x - bar_x;
+    *var = max(0, *var);
+    *var = min(*var, bar_w);
+    if(update_chosen_team) {
+        chosen_team = *var < bar_w / 2.0 ? 1 : 0;
+    }
 }
--- source/gameplay.h
+++ source/gameplay.h
@@ -12,6 +12,10 @@ private:
     unsigned char game_mode;
     bool mouse_on_ok_button;
     unsigned char chosen_team;
+    int picker_team_x;
+    bool mouse_down_on_team_picker;
+    int picker_unclaimed_x;
+    bool mouse_down_on_unclaimed_picker;
     
     ALLEGRO_BITMAP* bmp_button_1;
     ALLEGRO_BITMAP* bmp_button_2;
@@ -25,20 +29,49 @@ private:
     const int OK_BUTTON_X = WINDOW_WIDTH - OK_BUTTON_W - 8;
     const int OK_BUTTON_Y = 8;
     
-    const int PICKER_1_W = 750;
-    const int PICKER_1_H = 94;
-    const int PICKER_1_X = 25;
-    const int PICKER_1_Y = WINDOW_HEIGHT - 8 - PICKER_1_H;
+    const int PICKER_B_W = 750;
+    const int PICKER_B_H = 94;
+    const int PICKER_B_X = 25;
+    const int PICKER_B_Y = WINDOW_HEIGHT - 8 - PICKER_B_H;
     
-    const int PICKER_1_TEAM_BUTTON_W = 328;
-    const int PICKER_1_TEAM_BUTTON_H = 70;
-    const int PICKER_1_TEAM_BUTTON_Y = PICKER_1_Y + 13;
-    const int PICKER_1_TEAM_1_BUTTON_X = PICKER_1_X + 32;
-    const int PICKER_1_TEAM_2_BUTTON_X = PICKER_1_X + 390;
+    const int PICKER_B_TEAM_BUTTON_W = 328;
+    const int PICKER_B_TEAM_BUTTON_H = 70;
+    const int PICKER_B_TEAM_BUTTON_Y = PICKER_B_Y + 13;
+    const int PICKER_B_TEAM_1_BUTTON_X = PICKER_B_X + 32;
+    const int PICKER_B_TEAM_2_BUTTON_X = PICKER_B_X + 390;
+    
+    const int PICKER_I_W = 750;
+    const int PICKER_I_H = 94;
+    const int PICKER_I_X = 25;
+    const int PICKER_I_Y = WINDOW_HEIGHT - 8 - PICKER_I_H;
+    
+    const int PICKER_I_BAR_W = 684;
+    const int PICKER_I_BAR_H = 68;
+    const int PICKER_I_BAR_X = PICKER_I_X + 33;
+    const int PICKER_I_BAR_Y = PICKER_I_Y + 13;
+    
+    const int PICKER_E_W = 750;
+    const int PICKER_E_H = 151;
+    const int PICKER_E_X = 25;
+    const int PICKER_E_Y = WINDOW_HEIGHT - 8 - PICKER_E_H;
+    
+    const int PICKER_E_TEAM_BAR_W = 684;
+    const int PICKER_E_TEAM_BAR_H = 68;
+    const int PICKER_E_TEAM_BAR_X = PICKER_E_X + 33;
+    const int PICKER_E_TEAM_BAR_Y = PICKER_E_Y + 13;
+    
+    const int PICKER_E_NONE_BAR_W = PICKER_E_TEAM_BAR_W;
+    const int PICKER_E_NONE_BAR_H = 44;
+    const int PICKER_E_NONE_BAR_X = PICKER_E_TEAM_BAR_X;
+    const int PICKER_E_NONE_BAR_Y = PICKER_E_Y + 94;
     
     void draw_ink_button(
         const int x, const int y, const int w, const int h,
         ALLEGRO_COLOR color, const bool moving
+    );
+    void update_picker_x(
+        int* var, const int mouse_x,
+        const int bar_x, const int bar_w, const bool update_chosen_team
     );
     
 public:
